c7e618467d43f1b07f33f9f458f5af515f4749c0
Renzenar <118459019+Renzenar@users.noreply.github.com>
2024-11-02 17:09:04 -0500

    Tables

---
+++ Update_Database/Managers.py
@@ -0,0 +1,34 @@
+# Updates the Managers table
+import pandas as pd;
+
+def updateManagers(cursor):
+    print("Adding new managers...")
+
+    data=pd.read_csv("csvFiles/Managers.csv", na_values=['', ' '])
+    data = data.where(pd.notnull(data), None)
+    
+    managersAdded = 0
+
+    for row in data.iloc:
+        if(row["yearID"]<2023): 
+            continue
+
+        newManager = [
+            row['playerID'],
+            row['yearID'],
+            row['teamID'],
+            row['inseason'],
+            row['G'],
+            row['W'],
+            row['L'],
+            row['rank'],
+            row['plyrMgr']
+        ]
+        
+        sql = '''INSERT INTO managers (playerID, yearID, teamID, inSeason, manager_G, manager_W, manager_L, teamRank, plyrMgr)
+                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'''
+        #execute html and extract needed info from result set
+        exe = cursor.execute(sql,newManager)
+        managersAdded += exe
+    
+    print("Complete: {} new Managers added!".format(managersAdded))
---
+++ Update_Database/Schools.py
@@ -0,0 +1,28 @@
+# Updates the Schools table
+import pandas as pd;
+
+def updateSchools(cursor):
+    print("Adding new school data...")
+
+    columns = ["schoolID", "name_full", "city", "state", "country"]
+    data=pd.read_csv("csvFiles/Schools.csv", na_values=['', ' '], encoding='ISO-8859-1', usecols=columns)
+    data = data.where(pd.notnull(data), None)
+    
+    schoolsAdded = 0
+
+    for row in data.iloc:
+
+        newSchool = [
+            row['schoolID'],
+            row['name_full'],
+            row['city'],
+            row['state'],
+            row['country']
+        ]
+        
+        sql = '''INSERT IGNORE INTO schools (schoolID, school_name, school_city, school_state, school_country)
+                VALUES(%s,%s,%s,%s,%s);'''
+        exe = cursor.execute(sql,newSchool)
+        schoolsAdded += exe
+    
+    print("Complete: {} new rows of school data added!".format(schoolsAdded))
