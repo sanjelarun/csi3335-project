c7ed216db7512c1096045f7751e515b6409f9e8a
SamuelF2 <samueltfries@gmail.com>
2024-11-11 19:12:01 -0600

    Batting stats tested

--- Team_Roster/pages/roster.py
+++ Team_Roster/pages/roster.py
@@ -1,4 +1,9 @@
 from flask import render_template
+from app import db
+from app.models import Batting, People
+from sqlalchemy import func
+
+
 def getBattingStats(teamId, year):
     subquery = (
         db.session.query(
@@ -7,39 +12,34 @@ def getBattingStats(teamId, year):
             func.sum(Batting.b_AB).label("PA"),
             func.sum(Batting.b_HR).label("HR"),
             func.sum(Batting.b_SB).label("SB"),
-            func.div(func.sum(Batting.b_BB), func.sum(Batting.b_AB)).label("BB%"),
-            func.div(func.sum(Batting.b_SO), func.sum(Batting.b_AB)).label("K%"),
-            func.div(
-                func.sum(Batting.b_2B) + (2 * func.sum(Batting.b_3B)) + (3 * func.sum(Batting.b_HR)),
+            (func.sum(Batting.b_BB)/ func.sum(Batting.b_AB)).label("BB"),
+            (func.sum(Batting.b_SO)/ func.sum(Batting.b_AB)).label("K"),
+            (func.sum(Batting.b_2B) + (2 * func.sum(Batting.b_3B)) + (3 * func.sum(Batting.b_HR))/
                 func.sum(Batting.b_AB)).label("ISO"),
-            func.div(func.sum(Batting.b_H) - func.sum(Batting.b_HR),
-                     func.sum(Batting.b_AB) - (func.div(func.sum(Batting.b_SO),
-                                                        func.sum(Batting.b_AB))) - func.sum(
-                         Batting.b_HR) + func.sum(Batting.b_SF)).label("BABIP"),
-            func.div(
-                func.sum(Batting.b_H),
+            (func.sum(Batting.b_H) - func.sum(Batting.b_HR)/
+                func.sum(Batting.b_AB) - (func.sum(Batting.b_SO)/func.sum(Batting.b_AB)) - func.sum(Batting.b_HR) + func.sum(Batting.b_SF)
+                     ).label("BABIP"),
+            (func.sum(Batting.b_H)/
                 func.sum(Batting.b_AB)).label("AVG"),
-            func.div(
-                func.sum(Batting.b_H) + func.sum(Batting.b_BB) + func.sum(Batting.b_HBP),
+            (func.sum(Batting.b_H) + func.sum(Batting.b_BB) + func.sum(Batting.b_HBP)/
                 func.sum(Batting.b_AB) + func.sum(Batting.b_HBP) + func.sum(Batting.b_SF)
             ).label("OBP"),
-            func.div(
-                func.sum(Batting.b_H) + (2 * func.sum(Batting.b_2B)) + (3 * func.sum(Batting.b_3B)) + (
-                    func.sum(Batting.b_HR)),
+            (func.sum(Batting.b_H) + (2 * func.sum(Batting.b_2B)) + (3 * func.sum(Batting.b_3B)) + (
+                    func.sum(Batting.b_HR))/
                 func.sum(Batting.b_AB)
             ).label("SLG"),
-            func.div(
+            (
                 (0.69 * func.sum(Batting.b_BB)) +
                 (0.72 * func.sum(Batting.b_HBP)) +
                 (0.888 * func.sum(Batting.b_H)) +
                 (1.271 * func.sum(Batting.b_2B)) +
                 (1.616 * func.sum(Batting.b_3B)) +
-                (2.101 * func.sum(Batting.b_HR)),
-                (func.sum(Batting.b_AB)) +
-                (func.sum(Batting.b_BB)) -
-                (func.sum(Batting.b_IBB)) +
-                (func.sum(Batting.b_SF)) +
-                (func.sum(Batting.b_HBP))
+                (2.101 * func.sum(Batting.b_HR))/
+                func.sum(Batting.b_AB) +
+                func.sum(Batting.b_BB) -
+                func.sum(Batting.b_IBB) +
+                func.sum(Batting.b_SF) +
+                func.sum(Batting.b_HBP)
             ).label("wOBA"),
             # func.sum().label("wRC+"),  # Unsure
             # func.sum().label("BsR"),  # Unsure
@@ -58,11 +58,11 @@ def getBattingStats(teamId, year):
             func.concat(People.nameFirst, ' ', People.nameLast).label("full_name"),
             subquery.c.player_id,
             subquery.c["G"],
-            subquery.c["PA"],
-            subquery.c["HR"],
-            subquery.c["SB"],
-            subquery.c["BB%"],
-            subquery.c["K%"],
+            subquery.c.PA,
+            subquery.c.HR,
+            subquery.c.SB,
+            subquery.c["BB"],
+            subquery.c["K"],
             subquery.c["ISO"],
             subquery.c["BABIP"],
             subquery.c["AVG"],
@@ -85,17 +85,17 @@ def getBattingStats(teamId, year):
         player_data = {
             "full_name": result.full_name,
             "player_id": result.player_id,
-            "PA": result["PA"],
-            "HR": result["HR"],
-            "SB": result["SB"],
-            "BB%": result["BB%"],
-            "K%": result["K%"],
-            "ISO": result["ISO"],
-            "BABIP": result["BABIP"],
-            "AVG": result["AVG"],
-            "OBP": result["OBP"],
-            "SLG": result["SLG"],
-            "wOBA": result["wOBA"],
+            "PA": result.PA,
+            "HR": result.HR,
+            "SB": result.SB,
+            "BB%": result.BB,
+            "K%": result.K,
+            "ISO": result.ISO,
+            "BABIP": result.BABIP,
+            "AVG": result.AVG,
+            "OBP": result.OBP,
+            "SLG": result.SLG,
+            "wOBA": result.wOBA,
             # "wRC+": result["wRC+"],
             # "BsR": result["BsR"],
             # "Off": result["Off"],
