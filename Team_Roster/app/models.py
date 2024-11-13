7fd49376ecf176976da39c1a1909e1ad8b5fbb66
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-11-13 14:17:01 -0600

    Created basic system for logging in. Currently working on saving users and adding admin

--- Team_Roster/app/models.py
+++ Team_Roster/app/models.py
@@ -1,8 +1,34 @@
-
 from sqlalchemy import CHAR, Column, Date, Double, Integer, SmallInteger, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from typing import Optional
 
 Base = declarative_base()
+
+class User(UserMixin,db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 class Team(Base):
     __tablename__ = 'teams'
@@ -79,7 +105,6 @@ class Franchise(Base):
 
     def __repr__(self):
         return '<Franchise: {}, ID:{}, Active: {}>'.format(self.franchName,self.franchID,self.active)
-    
 
 class Fielding(Base):
     __tablename__ = 'fielding'
--- Team_Roster/app/routes.py
+++ Team_Roster/app/routes.py
@@ -1,22 +1,78 @@
-from flask import request, render_template
-from app import app
 from pages.findTeam import ShowFindTeam
 from pages.roster import ShowRoster
 from pages.depthChart import ShowDepthChart
+from flask import flash, redirect, url_for, render_template, request
+from flask_login import current_user, login_user, logout_user, login_required
+from urllib.parse import urlsplit
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+import sqlalchemy as sa
 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    return ShowFindTeam()
 
-@app.route('/',methods=['GET', 'POST'])
-@app.route('/index',methods=['GET', 'POST'])
 @app.route('/findTeam', methods=['GET', 'POST'])
+@login_required
 def findTeam():
     return ShowFindTeam()
 
-@app.route('/<teamId>/roster',methods=['GET'])
+@app.route('/<teamId>/roster', methods=['GET'])
+@login_required
 def roster(teamId):
     year = request.args.get("year")
     return ShowRoster(teamId, year)
 
 @app.route('/<teamId>/depthChart', methods=['GET'])
+@login_required
 def depthChart(teamId):
     year = request.args.get("year")
     return ShowDepthChart(teamId, year)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        # Check if user exists and if the password is correct
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+
+        # Redirect to the next page if specified, otherwise to index
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
