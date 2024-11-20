c35ed98d399f4827d700d1564898c6a70ba479f2
Jarpletz <rothpletz5@gmail.com>
2024-11-19 18:50:40 -0600

    Created functions to perform calculations in

--- Team_Roster/pages/immaculateGrid.py
+++ Team_Roster/pages/immaculateGrid.py
@@ -1,5 +1,7 @@
 from flask import render_template, request
 from app.forms import ImmaculateGridInput
+from immaculateGridCalculations.webScraper import scrapeImmaculateGridQuestions
+from immaculateGridCalculations.gridSolver import solveGrid
 
 def ShowImmaculateGrid():
     form = ImmaculateGridInput()
@@ -9,11 +11,7 @@ def ShowImmaculateGrid():
 
     if form.validate_on_submit():
         url = form.url.data
-        questions = ["Detroit Tigers","Boston Red Sox","Season RBI 500+","World Series Winner", "Seattle Mariners", "Played 1st Base +1 Game"]
-        solutionNames = ["Babe Ruth", "Also Babe Ruth","Babe Ruth Once Again",
-                         "Logan Rigdon","Samuel Fries","August Rothpletz",
-                         "Mitchell Thompson","Uhhhhhh Speegle","500 Cent Ice Tea DOOM"]
-
-        
+        questions = scrapeImmaculateGridQuestions(url)
+        solutionNames = solveGrid(questions)
     
     return render_template('immaculateGrid.html', title='Immaculate Grid Solver', form=form, url = url,questions = questions, solutionNames = solutionNames)
