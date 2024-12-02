d4f8752e71c08b7e7b64967d93d4a48961b30eda
Jarpletz <rothpletz5@gmail.com>
2024-12-02 12:23:09 -0600

    FUCKING GOT IT AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Team_Roster/pages/roster.py
+++ Team_Roster/pages/roster.py
@@ -2,20 +2,59 @@ from flask import render_template
 from app import db
 import sqlalchemy as sa
 from app.models import Batting, People, Team, Season, Fielding
-from sqlalchemy import and_, func
+from sqlalchemy import and_, func, case
+
+
 
 
 def getBattingStats(teamId, year):
+
+    grouped_fielding = (
+        db.session.query(
+            Fielding.playerID.label("playerID"),
+            Fielding.yearID.label("yearID"),
+            Fielding.teamID.label("teamID"),
+            Fielding.stint.label("stint"),
+            Fielding.position.label("position"),
+            Fielding.f_G.label("f_G"),
+            Fielding.f_GS.label("f_GS"),
+            Fielding.f_InnOuts.label("f_InnOuts"),
+            Fielding.f_PO.label("f_PO"),
+            Fielding.f_A.label("f_A"),
+        )
+        .group_by(Fielding.playerID, Fielding.yearID, Fielding.teamID, Fielding.stint)  # Ensure to group by all non-aggregated fields
+        .subquery()
+    )
+    position_adjustments = { #used for war
+    'C': 9,
+    '1B': -9.5,
+    '2B': 3,
+    '3B': 2,
+    'SS': 7,
+    'LF': -7,
+    'CF': 2.5,
+    'RF': -7,
+    }
+    # Create a SQLAlchemy case expression
+
+    adjustment_case = case(
+        *[
+            (grouped_fielding.c.position == position, adjustment * grouped_fielding.c.f_InnOuts/3)
+            for position, adjustment in position_adjustments.items()
+        ],
+        else_=0  # Default adjustment if position doesn't match
+    )
+
     subquery = (
         db.session.query(
             Batting.playerID.label("player_id"),
             func.sum(Batting.b_G).label("G"),
             (
-                (Batting.b_AB)+
-                (Batting.b_BB)+
-                (Batting.b_HBP)+
-                (Batting.b_SH)+
-                (Batting.b_SF)
+                func.sum(Batting.b_AB)+
+                func.sum(Batting.b_BB)+
+                func.sum(Batting.b_HBP)+
+                func.sum(Batting.b_SH)+
+                func.sum(Batting.b_SF)
             ).label("PA"),
             func.sum(Batting.b_HR).label("HR"),
             func.sum(Batting.b_SB).label("SB"),
@@ -82,51 +121,160 @@ def getBattingStats(teamId, year):
                     )
                 )
                 + 
-                ( #BsR= wGDP + wSB
-                    # wGDP = (((lgGDP / lgGDPo) * GDPo) - GDP ) * lgRPO
-                    # (
-                    #     (
-                    #         (Season.s_ / lgGDPo) 
-                    #         * GDPo
-                    #     ) 
-                    #     - 
-                    #     GDP 
-                    # ) 
-                    # * lgRPO
-                    # (func.sum(Batting.b_SB) * Season.s_runSB)
-                    # +
-                    # (func.sum(Batting.b_CS) * Season.s_runCS)
+                ( #BsR = wSB=((SB*runSB)+(CS*runCS))-(lgwSB*(1B+BB+HBP-IBB))
+                    (
+                       (func.sum(Batting.b_SB) * Season.s_runSB)
+                       + 
+                       (func.sum(Batting.b_CS) * Season.s_runCS)
+                    ) 
+                    -
+                    (
+                        (# lgwSB=((lgSB*runSB)+(lgCS*runCS))/(lg1B+lgBB+lgHBP+lgIBB)
+                            (
+                                (Season.s_SB * Season.s_runSB) + 
+                                (Season.s_CS * Season.s_runCS)
+                            )
+                            /
+                            (
+                                Season.s_1B + Season.s_BB + Season.s_HBP - Season.s_IBB
+                            )
+                        )
+                        *
+                        (
+                            (func.sum(Batting.b_H) - (func.sum(Batting.b_2B) + func.sum(Batting.b_3B) + func.sum(Batting.b_HR))) #1B
+                            +
+                            func.sum(Batting.b_BB)
+                            +
+                            func.sum(Batting.b_HBP)
+                            -
+                            func.sum(Batting.b_IBB)
+                        )
+                    )
                 )
                 +
-                ( # Fielding Runs= Range Runs+Error Runs + Double Play Runs
-                    0
-                    #func.sum(Fielding.f_ZR)
-                    # + Error runs
-                    # + Double play controbutions
+                ( # Positional Adjustment
+                   func.sum(adjustment_case) / 1350
+                )
+                +
+                ( # RLR = ((0.235 * G) * RPW * PA) / lgPA
+                    (
+                        (0.235 * Season.s_G/2)
+                        * 
+                        Season.s_R_W 
+                        *
+                        #PA = AB+BB+HBP+SF+SH
+                        (func.sum(Batting.b_AB)+
+                        func.sum(Batting.b_BB)+
+                        func.sum(Batting.b_HBP)+
+                        func.sum(Batting.b_SH)+
+                        func.sum(Batting.b_SF))
+                    )
+                    /
+                    Season.s_PA
+                )
+                )
+                /Season.s_R_W
+            ).label("WAR")  ,
 
-                )
-                # + Positional Adjustment
-                +
-                ( # Replacement Runs = 20/600 * PA
-                    (20/600)
+            #####
+            #####
+            ( #wRAA = (wOBA - leagueWOBA) / wOBAScale * AB+BB+HBP+SF+SH
+                    ((
+                    (#wOBA (took logan's equation)
+                        ((Season.s_wBB * (func.sum(Batting.b_BB) - func.sum(Batting.b_IBB))) +
+                        (Season.s_wHBP * func.sum(Batting.b_HBP)) +
+                        (Season.s_w1B * ((func.sum(Batting.b_H) - (func.sum(Batting.b_2B) + func.sum(Batting.b_3B) + func.sum(Batting.b_HR))))) +
+                        (Season.s_w2B * func.sum(Batting.b_2B)) +
+                        (Season.s_w3B * func.sum(Batting.b_3B)) +
+                        (Season.s_wHR * func.sum(Batting.b_HR)))
+                        /
+                        (func.sum(Batting.b_AB) +
+                        (func.sum(Batting.b_BB) -
+                        func.sum(Batting.b_IBB)) +
+                        func.sum(Batting.b_SF) +
+                        func.sum(Batting.b_HBP))
+                    )
+                    - Season.s_wOBA
+                    )
+                    / Season.s_wOBAScale)
                     * ( #PA = AB+BB+HBP+SF+SH
+                        func.sum(Batting.b_AB) +
+                        func.sum(Batting.b_BB) +
+                        func.sum(Batting.b_SF) +
+                        func.sum(Batting.b_HBP) +
+                        func.sum(Batting.b_SH)
+                    )
+            ).label("wRAA"),
+
+            ( #BsR = wSB=((SB*runSB)+(CS*runCS))-(lgwSB*(1B+BB+HBP-IBB))
+                    (
+                       (func.sum(Batting.b_SB) * Season.s_runSB)
+                       + 
+                       (func.sum(Batting.b_CS) * Season.s_runCS)
+                    ) 
+                    -
+                    (
+                        (# lgwSB=((lgSB*runSB)+(lgCS*runCS))/(lg1B+lgBB+lgHBP+lgIBB)
+                            (
+                                (Season.s_SB * Season.s_runSB) + 
+                                (Season.s_CS * Season.s_runCS)
+                            )
+                            /
+                            (
+                                Season.s_1B + Season.s_BB + Season.s_HBP - Season.s_IBB
+                            )
+                        )
+                        *
+                        (
+                            (func.sum(Batting.b_H) - (func.sum(Batting.b_2B) + func.sum(Batting.b_3B) + func.sum(Batting.b_HR))) #1B
+                            +
+                            func.sum(Batting.b_BB)
+                            +
+                            func.sum(Batting.b_HBP)
+                            -
+                            func.sum(Batting.b_IBB)
+                        )
+                    )
+                ).label("BsR"),
+                ( # Positional Adjustment
+                   func.sum(adjustment_case) / 1350
+                ).label("PositionalAdj"),
+                ( # RLR = ((0.235 * G) * RPW * PA) / lgPA
+                    (
+                        (0.235 * Season.s_G)
+                        * 
+                        Season.s_R_W 
+                        *
+                        #PA = AB+BB+HBP+SF+SH
                         (func.sum(Batting.b_AB) +
                         func.sum(Batting.b_BB) +
                         func.sum(Batting.b_HBP) +
                         func.sum(Batting.b_SF) +
                         func.sum(Batting.b_SH))
                     )
-                )
-                )
-                /Season.s_R_W
-            ).label("WAR")  
+                    /
+                    Season.s_PA
+                ).label("RLR")
 
+        )       
+        .join(grouped_fielding,and_(
+            Batting.playerID == grouped_fielding.c.playerID,
+            Batting.yearID == grouped_fielding.c.yearID,
+            Batting.teamID == grouped_fielding.c.teamID,
+            Batting.stint == grouped_fielding.c.stint
+        
+        ))
+        .join(Season,Season.yearID == Batting.yearID)
+        .filter(
+            Batting.yearID == year,
+            Batting.teamID == teamId,
+            Season.yearID == year
         )
-        .filter(Batting.yearID == year, Batting.teamID == teamId, Season.yearID == year, Fielding.yearID==year)
-        .join(Fielding, Batting.playerID == Fielding.playerID)
         .group_by(Batting.playerID)
         .subquery()
     )
+
+    print(subquery)
 
     results = (
         db.session.query(
@@ -148,10 +296,14 @@ def getBattingStats(teamId, year):
             # subquery.c["BsR"],
             # subquery.c["Off"],
             # subquery.c["Def"],
-            subquery.c["WAR"]
+            subquery.c["WAR"],
+            subquery.c["wRAA"],
+            subquery.c["BsR"],
+            subquery.c["PositionalAdj"],
+            subquery.c["RLR"],
         )
         .join(People, People.playerID == subquery.c.player_id)
-        .order_by(subquery.c.PA.desc())
+        .order_by(subquery.c.WAR.desc())
         .all()
     )
 
@@ -177,8 +329,15 @@ def getBattingStats(teamId, year):
             # "Off": result["Off"],
             # "Def": result["Def"],
             "WAR": result.WAR,
+            "wRAA": result.wRAA,
+            "BsR": result.BsR,
+            "PositionalAdj": result.PositionalAdj,
+            "RLR": result.RLR
         }
         batting_data[result.player_id] = player_data
+
+        if result.full_name == "Josh Harrison":
+            print(player_data)
 
     return batting_data
 
