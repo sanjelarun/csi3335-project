c7dbc4c5878136ae87070d7fc92b5f40638466fb
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-12-04 19:44:36 -0600

    Saves each answer to queries for a given user

--- pages/immaculateGrid.py
+++ pages/immaculateGrid.py
@@ -1,7 +1,11 @@
 from flask import render_template, request
+from flask_login import current_user
 from app.forms import ImmaculateGridInput
 from immaculateGridCalculations.webScraper import scrapeImmaculateGridQuestions
 from immaculateGridCalculations.gridSolver import solveGrid
+
+from app.models import Queries
+from app import db
 
 
 def ShowImmaculateGrid():
@@ -14,6 +18,15 @@ def ShowImmaculateGrid():
         url = form.url.data
         questions = scrapeImmaculateGridQuestions(url)
         solutionNames = solveGrid(questions)
+
+        for i in range(3):  # 3 rows
+            for j in range(3):  # 3 columns
+                q_questions = questions[j] + " & " + questions[i + 3]
+                q_solutions = solutionNames[i * 3 + j]
+
+                query = Queries(user_ID=current_user.get_id(), q_QUESTIONS=q_questions, q_SOLUTIONS=q_solutions)
+                db.session.add(query)
+                db.session.commit()
 
         print("URL:", url)
         print("Questions:", questions)
